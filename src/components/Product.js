afd72cfe2f001877d99284f297ca556372102ee2
ajhar10 <ajharrimon90@gmail.com>
2022-08-19 23:45:00 +0600

    all set

--- src/components/Product.js
+++ src/components/Product.js
@@ -5,19 +5,20 @@ import { addToCart } from '../redux/actions'
 const Product = ({productData, addToCart}) => {
     const items = useSelector((state)=>state.cart)
     const item = items?.find(item=>item?.id===productData.id)
-    
+    const count  = item? productData.qty-item.qty : productData.qty
+    console.log(count);
   return (
     <div
                         class="bg-white py-4 px-4 shadow-md rounded-lg my-4 mx-4"
                     >
                         <div class="flex justify-between px-4 items-center">
                             <div class="text-lg font-semibold">
-                                <p>{productData.name} ({item? productData.qty-item.qty : productData.qty})</p>
+                                <p>{productData.name} ({count})</p>
                                 <p class="text-gray-400 text-base">Tk {productData.price}</p>
                             </div>
                             <div class="text-lg font-semibold">
-                                <button onClick={()=>addToCart(productData.id)}
-                                    class="focus:outline-none bg-purple-700 hover:bg-purple-800 text-white font-bold py-2 px-2 rounded-full inline-flex items-center"
+                                <button disabled={count===0 ? true: false}  onClick={()=>addToCart(productData.id)}
+                                    class="disable focus:outline-none bg-purple-700 hover:bg-purple-800 text-white font-bold py-2 px-2 rounded-full inline-flex items-center"
                                 >
                                     <svg
                                         xmlns="http://www.w3.org/2000/svg"
